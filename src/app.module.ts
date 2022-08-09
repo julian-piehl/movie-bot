de2a8c4c22e8d85f316c093c170ddf6e60fc99ce
Peace <32363424+julian-piehl@users.noreply.github.com>
2022-08-09 20:30:56 +0200

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Intents } from 'discord.js';
+import { NecordModule } from 'necord';
+import { AppUpdate } from './app.update';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'postgres',
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    NecordModule.forRoot({
+      token: process.env.DISCORD_TOKEN,
+      intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+      development: [process.env.DISCORD_DEV_GUILD_ID],
+    }),
+  ],
+  providers: [AppUpdate],
+})
+export class AppModule {}
