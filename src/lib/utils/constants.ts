326a54fce76272607c599c5bbc071730bfa962a6
Peace <Debitor.TooLP@gmx.de>
2023-11-18 18:02:56 +0100

    fix: limit votes env

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -5,6 +5,4 @@ export const LogChannelID = process.env.LOG_CHANNEL_ID || null;
 
 export const TmdbID = process.env.TMDB_ID || null;
 
-export const LimitVotes = isNullish(process.env.MOVIEBOT_LIMIT_VOTES)
-  ? true
-  : Boolean(process.env.MOVIEBOT_LIMIT_VOTES);
+export const LimitVotes = isNullish(process.env.MOVIEBOT_LIMIT_VOTES) ? true : process.env.MOVIEBOT_LIMIT_VOTES;
